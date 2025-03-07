afdda66b334b0a49dca01c9a4240e460a7eda5c4
Arjan Timmer <arjan.timmer@ns.nl>
2025-03-07 15:43:00 +0100

    Refactor Sanity configuration to remove default values for dataset and projectId

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const config = {
-  dataset: process.env.SANITY_STUDIO_DATASET || 'production',
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
+  dataset: process.env.SANITY_STUDIO_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
 };
