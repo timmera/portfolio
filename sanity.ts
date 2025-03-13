210f627c194cf89b742fbc455c2e908268c7ac44
Arjan Timmer <arjan.timmer@ns.nl>
2025-03-13 10:43:38 +0100

    Remove default value for Sanity

--- sanity.ts
+++ sanity.ts
@@ -12,7 +12,7 @@ if (
 
 export const config = {
   dataset: process.env.SANITY_STUDIO_DATASET || 'production',
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID || 't4s3mk7z',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
 };
