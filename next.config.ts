e6c160d706a7339a96f95aea83f63b6f46536148
Arjan Timmer <arjan.timmer@ns.nl>
2025-03-13 12:01:23 +0100

    Add reactComponentAnnotation configuration to next.config.ts and remove it from sanity.ts

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  reactComponentAnnotation: {
+    enabled: process.env.NODE_ENV === 'development',
+  },
 };
 
 export default nextConfig;
